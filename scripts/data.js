ef7ecfab9e25909b11e4dfe0803722a4b27a1659
Ynah Umayam <ynah.rosal.umayam@gmail.com>
2026-09-04 22:55:11 +0800

    add hero image and resume file

--- scripts/data.js
+++ scripts/data.js
@@ -3,7 +3,7 @@ export let techStack = {};
 export let experiences = [];
 
 export function loadPortfolioData() {
-  return fetch('../data.json')
+  return fetch('data.json')
   .then((response) => {
     return response.json();
   }).then((data) => {
